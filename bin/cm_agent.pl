fd201a8c8cf74e4702dccf682b7c3690871a69f5
Kalin Nikolov <knikolov@paypal.com>
2013-11-24 17:33:41 -0800

    modified the agent so that rules can be specified in Rules.pm

--- bin/cm_agent.pl
+++ bin/cm_agent.pl
@@ -1,16 +1,41 @@
 #!/usr/bin/perl
 
+my $base_dir = "/x/itools";
+
 use File::Path qw( make_path );
 use DBI;
+use Getopt::Long;
 use strict;
+require "$base_dir/conf/Rules.pm";
 
-my $base_dir = "/x/itools";
 my $conf_file = "${base_dir}/conf/cm_agent.cfg";
 my $conf_ref = get_conf();
 my %Conf = %$conf_ref;
 
+my $Rules_ref = Rules::GetAllRules();
+my %Rules = %$Rules_ref;
+
+my $GuestMountRuleExists = 0;
+my $HypervisorRuleExists = 0;
+
+for my $key (keys %Rules) {
+    if ( $Rules{$key}{Type} eq "GuestMount" ) {
+         $GuestMountRuleExists = 1;
+    }
+    elsif ( $Rules{$key}{Type} eq "Hypervisor" ) {
+         $HypervisorRuleExists = 1;
+    }
+}
+
+my $debug;
+GetOptions( 'debug'   => \$debug );
+
+my $rules_boolean_operator = "or"; # Default operator
+if ( $Conf{rules_boolean_operator} ) {
+   $rules_boolean_operator = $Conf{rules_boolean_operator};
+}
+
 my $current_time = time();
-my $unused_login_sec = ( $Conf{unused_last_login_days} * 86400 );
 
 my $compute_host = `hostname`;
 chomp($compute_host);
@@ -19,26 +44,56 @@ my $instances_ref = GetInstances();
 my %Instances = %$instances_ref;
 
 my $dbh = DBI->connect("DBI:mysql:database=$Conf{lifetime_db};host=$Conf{db_host};port=$Conf{db_port}", "$Conf{lifetime_db_user}", "$Conf{lifetime_db_password}",
-          {'RaiseError' => 1 });    
+          {'RaiseError' => 1 });
 
 
 foreach my $domain (keys %Instances) {
     my $uuid = $Instances{$domain};
-
+    my %UnusedResult1 = ();
+    my %UnusedResult2 = ();
+    my $final_result;
     my $expiration_date = GetVMExpirationDate($uuid);
+
     if ( ! $expiration_date ) {
         print "Checking $domain ($uuid)\n";
-        my $unused = GuestMountAndCheck($domain);
-        if ($unused == 1 ) {
-             print "There was error checking $domain\n";
+        if ( $GuestMountRuleExists == 1 ) {
+            my $UnusedResult_ref = GuestMountAndCheck($domain);
+            %UnusedResult1 = %$UnusedResult_ref;
         }
-        else {
-             UpdateDB($uuid, $unused);
+        if ( $HypervisorRuleExists == 1 ) {
+            my $UnusedResult_ref = HypervisorCheck($domain);
+            %UnusedResult2 = %$UnusedResult_ref;
+        }
+
+        my %UnusedResult = (%UnusedResult1, %UnusedResult2);
+        for my $key ( keys %UnusedResult ) {
+            print "DEBUG: RuleResult: $key = $UnusedResult{$key}\n" if ($debug);
+            if ( ! $final_result ) {
+                $final_result = $UnusedResult{$key};
+                next;
+            }
+            if ( $rules_boolean_operator eq "or" ) {
+                if ( $final_result eq "true" or $UnusedResult{$key} eq "true" ) {
+                    $final_result = "true";
+                }
+            } elsif ( $rules_boolean_operator eq "and" ) {
+                if ( $final_result eq "true" and $UnusedResult{$key} eq "true" ) {
+                    $final_result = "true";
+                } else {
+                    $final_result = "false";
+                }
+            }
+        }
+        if ( $final_result ) {
+           UpdateDB($uuid, $final_result);
+           print "DEBUG: uuid = $final_result\n" if ($debug);
         }
     }
     else {
         print "Instance: $domain ($uuid) has an expiration date\n";
     }
+
+    #last;
 }
 
 $dbh->disconnect();
@@ -48,8 +103,8 @@ $dbh->disconnect();
 
 sub GetInstances {
     my %Instances = ();
-    
-    my @LibvirtXML_Files = `ls $Conf{instances_dir}/*/libvirt.xml`; 
+
+    my @LibvirtXML_Files = `ls $Conf{instances_dir}/*/libvirt.xml`;
     foreach my $xml_file ( @LibvirtXML_Files ) {
         chomp($xml_file);
         my ($domain, $uuid);
@@ -114,17 +169,17 @@ sub UpdateDB {
 ##############################################################################
 sub GuestMountAndCheck {
     my $domain = shift;
-    my $unused = "false";
-    my $wtmp_file = "$Conf{guestmount_dir}/var/log/wtmp";
+    my %UnusedResult = ();
 
     if ( !-d $Conf{guestmount_dir} ) {
         make_path $Conf{guestmount_dir} or die "Failed to create path: $Conf{guestmount_dir}";
-    } 
+    }
     # check whether guestmount dir is already mounted
     my $result = `cat /proc/mounts | grep $Conf{guestmount_dir}`;
     if ($result) {
         print "$Conf{guestmount_dir} is still mounted. The agent will exit.\n";
-        return(1);
+        #return(1);
+        return(\%UnusedResult);
     }
 
     # guestmount the domain
@@ -132,20 +187,25 @@ sub GuestMountAndCheck {
     my $result = `cat /proc/mounts | grep $Conf{guestmount_dir}`;
     if ( ! $result) {
         print "There was a problem mounting $domain\n";
-        return(1);
+        #return(1);
+        return(\%UnusedResult);
     }
 
-    #############################################################    
-    # Run various check on the mounted domain
+    #############################################################
+    # Run rules on the mounted domain
 
-    # check last login
-    if ( -f "$wtmp_file" ) {
-        my $file_mod_time = `stat -c %Y $wtmp_file`;
-        chomp($file_mod_time);
-        if ( $file_mod_time < ($current_time - $unused_login_sec ) ) {
-             $unused = "true";
+    for my $key (keys %Rules) {
+        my $unused = "false";
+        if ( $Rules{$key}{Type} eq "GuestMount" ) {
+            if ( $Rules{$key}{Function} ) {
+                $unused = RunFunction($domain, $Rules{$key}{Type}, $Rules{$key}{Function}, $Rules{$key}{FileName}, $Rules{$key}{UnusedCondition}, $Rules{$key}{UnusedValue} );
+            }
+            elsif ( $Rules{$key}{Command} ) {
+                $unused = RunCommand($domain, $Rules{$key}{Type}, $Rules{$key}{Command}, $Rules{$key}{UnusedCondition}, $Rules{$key}{UnusedValue} );
+            }
+            $UnusedResult{$key} = $unused;
         }
-    } 
+    }
 
     # unmount the doamin
     system("fusermount -u $Conf{guestmount_dir}");
@@ -153,10 +213,84 @@ sub GuestMountAndCheck {
     my $result = `cat /proc/mounts | grep $Conf{guestmount_dir}`;
     if ($result) {
         print "$Conf{guestmount_dir} is still mounted. The agent will exit.\n";
-        return(1);
+        return(\%UnusedResult);
     }
 
-    return($unused);
+    return(\%UnusedResult);
+}
+##############################################################################
+sub HypervisorCheck {
+    my $domain = shift;
+    my $unused = "false";
+    my %UnusedResult = ();
+    for my $key (keys %Rules) {
+        if ( $Rules{$key}{Type} eq "Hypervisor" ) {
+            $unused = RunCommand($domain, $Rules{$key}{Type}, $Rules{$key}{Command}, $Rules{$key}{UnusedCondition}, $Rules{$key}{UnusedValue} );
+            $UnusedResult{$key} = $unused;
+        }
+    }
+    return(\%UnusedResult);
+}
+##############################################################################
+sub RunCommand {
+    my $domain           = shift;
+    my $type             = shift;
+    my $command          = shift;
+    my $unused_condition = shift;
+    my $unused_value     = shift;
+    my $unused_result    = "false";
+
+    print "DEBUG: domain = $domain : type = $type : command = $command : cond = $unused_condition : value = $unused_value\n" if ($debug);
+    my $result = `$command $domain`;
+    chomp($result);
+    print "DEBUG: Command_Result = $result\n" if ($debug);
+
+    if ( $unused_condition eq "LessThan" ) {
+        if ( $result < $unused_value ) {
+            $unused_result = "true";
+        }
+    }
+    elsif ( $unused_condition eq "GreaterThan" ) {
+        if ( $result > $unused_value ) {
+            $unused_result = "true";
+        }
+    }
+    elsif ( $unused_condition eq "StringMatch" ) {
+        if ( $result eq "$unused_value" ) {
+            $unused_result = "true";
+        }
+    }
+    return ($unused_result);
+}
+##############################################################################
+sub RunFunction {
+    my $domain           = shift;
+    my $type             = shift;
+    my $function         = shift;
+    my $file_name        = shift;
+    my $unused_condition = shift;
+    my $unused_value     = shift;
+    my $unused_result    = "false";
+
+    print "DEBUG: domain = $domain : type = $type : function = $function : file = $file_name : cond = $unused_condition : value = $unused_value\n" if ($debug);
+
+    ###################################################
+    # If Function is FileModTime - if the file is older
+
+    if ( $function eq "FileModTime" ) {
+        my $GuestMountedFile = "$Conf{guestmount_dir}"."$file_name";
+        if ( -f $GuestMountedFile ) {
+            my $file_mod_time = `stat -c %Y $GuestMountedFile`;
+            chomp($file_mod_time);
+            if ( $unused_condition eq "DaysOlderThan" ) {
+                if ( $file_mod_time < ($current_time - ($unused_value * 86400)) ) {
+                    $unused_result = "true";
+                }
+            }
+        }
+    }
+    return ($unused_result);
+
 }
 ##############################################################################
 sub get_conf {
@@ -174,4 +308,3 @@ sub get_conf {
    }
    return (\%conf);
 }
-
