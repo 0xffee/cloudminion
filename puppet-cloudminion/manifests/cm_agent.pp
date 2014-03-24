24b10f5f23cdeec0bd4953d774df37cfbde20539
Kalin Nikolov <knikolov@paypal.com>
2014-03-24 12:51:27 -0700

    added lib dir

--- puppet-cloudminion/manifests/cm_agent.pp
+++ puppet-cloudminion/manifests/cm_agent.pp
@@ -2,6 +2,7 @@ class cloudminion::cm_agent(
    $cm_install_dir = '',
    $cm_conf_dir = "${cm_install_dir}/conf",
    $cm_bin_dir = "${cm_install_dir}/bin",
+   $cm_lib_dir = "${cm_install_dir}/lib",
 ) {
 
     schedule { 'once': 
@@ -23,6 +24,7 @@ class cloudminion::cm_agent(
         owner   => 'root',
         group   => 'root',
         mode    => '0775',
+        require => [ File['cm_install_dir']],
     }
     file {'cm_bin_dir':
         path => "$cm_bin_dir",
@@ -30,6 +32,15 @@ class cloudminion::cm_agent(
         owner   => 'root',
         group   => 'root',
         mode    => '0775',
+        require => [ File['cm_install_dir']],
+    }
+    file {'cm_lib_dir':
+        path => "$cm_lib_dir",
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0775',
+        require => [ File['cm_install_dir']],
     }
 
     file {"cm_agent.cfg":
@@ -39,7 +50,7 @@ class cloudminion::cm_agent(
         content => template("cloudminion/cm_agent.cfg.erb"),
         ensure => file,
         mode => '0664',
-        require => [ File['cm_conf_dir']]
+        require => [ File['cm_conf_dir']],
     }
 
     file {"Rules.pm":
@@ -49,7 +60,7 @@ class cloudminion::cm_agent(
         content => template("cloudminion/Rules.pm.erb"),
         ensure => file,
         mode => '0664',
-        #require => [ File['cm_conf_dir']]
+        require => [ File['cm_conf_dir']],
     }
 
     file {"cm_agent.pl":
@@ -59,14 +70,14 @@ class cloudminion::cm_agent(
         source => 'puppet:///modules/cloudminion/cm_agent.pl',
         ensure => file,
         mode => '0775',
-        require => [ File['cm_bin_dir']]
+        require => [ File['cm_bin_dir']],
     }
 
     exec {"run_cm_agent":
         command => "$cm_bin_dir/cm_agent.pl",
         timeout => 30,
         schedule => "once",
-        require => [ File['cm_agent.pl']]
+        require => [ File['cm_agent.pl']],
     }     
 
 }
